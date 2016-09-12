58ab11889d0fe5415389bd1caf45d03bdd322bf0
tracie-russell <ivorygirl@gmail.com>
2016-09-11 22:33:29 -0500

    Finished assignment

--- main.py
+++ main.py
@@ -14,12 +14,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import jinja2
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_front(self, title="", art="", error=""):
+        arts = db.GqlQuery("SELECT * from Art ORDER BY created DESC")
+        self.render("front.html", title=title, art=art, error=error, arts = arts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        art = self.request.get("art")
+
+        if title and art:
+            a = Art(title = title, art = art)
+            a.put()
+
+            self.redirect("/")
+        else:
+            error = "We need both a title and some artwork!"
+            self.render_front(title, art, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
